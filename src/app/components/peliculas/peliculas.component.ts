225e4bbdef5e5af00ee2426c6bdd82f9fe99e0e7
Daniel Gonzalez <dan.gzz@outlook.com>
2019-11-20 00:11:50 -0600

    Me quede en Clases

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -19,12 +19,12 @@ export class PeliculasComponent implements OnInit {
 
   getMovies() {
     this.peliculas = [
-      { titulo: 'Spiderman 1', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHQvqILPtAOdGe224vyidiP4gJh6Kkv6hoF8ZC5Vq68gGxgQPe' },
-      { titulo: 'Star Wars', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTCzXfErYlW_Af1vTVil5IGAjCcSlGwf79cSjt2FMXQyf0Rpzto' },
-      { titulo: 'Avengers', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ60i8ZIoMh38deQlfU0J19W-5GIzFYKC-vkDCEyvz34f0YEF2M' },
-      { titulo: 'Back to the future', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPUQ0DZ_4y4GlK1fzKeOMzPeAed8IgJakKjchbZb4ngkRNihL1' },
-      { titulo: 'Godzilla', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3YrHdrz4MTf2DcfRGatnQnGfxUz1JMG7PPX-THy-hc5PkKP7Q' },
-      { titulo: 'Lion King', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJwrrD3K1_oKzR9MYSvgFVs4RHBFrmWELvsC9RILag22QpIi9y' }
+      { year:2012, titulo: 'Spiderman 1', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHQvqILPtAOdGe224vyidiP4gJh6Kkv6hoF8ZC5Vq68gGxgQPe' },
+      { year:2018, titulo: 'Star Wars', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTCzXfErYlW_Af1vTVil5IGAjCcSlGwf79cSjt2FMXQyf0Rpzto' },
+      { year:2015, titulo: 'Avengers', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ60i8ZIoMh38deQlfU0J19W-5GIzFYKC-vkDCEyvz34f0YEF2M' },
+      { year:2010, titulo: 'Back to the future', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPUQ0DZ_4y4GlK1fzKeOMzPeAed8IgJakKjchbZb4ngkRNihL1' },
+      { year:2011, titulo: 'Godzilla', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3YrHdrz4MTf2DcfRGatnQnGfxUz1JMG7PPX-THy-hc5PkKP7Q' },
+      { year:2016, titulo: 'Lion King', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJwrrD3K1_oKzR9MYSvgFVs4RHBFrmWELvsC9RILag22QpIi9y' }
     ];
   }
 
