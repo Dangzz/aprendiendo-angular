ead13db42729d49744d4dee881e3983f4b099daa
Daniel Gonzalez <dan.gzz@outlook.com>
2019-11-22 01:08:52 -0600

    Se agrego el modelo Peliculas

--- src/app/components/peliculas/peliculas.component.ts
+++ src/app/components/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pelicula } from '../../models/pelicula.model';
 
 @Component({
   selector: 'app-peliculas',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  peliculas: Array<any>;
+  public peliculas: Array<Pelicula>;
 
   constructor() {
     this.getMovies();
@@ -19,12 +20,12 @@ export class PeliculasComponent implements OnInit {
 
   getMovies() {
     this.peliculas = [
-      { year:2012, titulo: 'Spiderman 1', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHQvqILPtAOdGe224vyidiP4gJh6Kkv6hoF8ZC5Vq68gGxgQPe' },
-      { year:2018, titulo: 'Star Wars', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTCzXfErYlW_Af1vTVil5IGAjCcSlGwf79cSjt2FMXQyf0Rpzto' },
-      { year:2015, titulo: 'Avengers', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ60i8ZIoMh38deQlfU0J19W-5GIzFYKC-vkDCEyvz34f0YEF2M' },
-      { year:2010, titulo: 'Back to the future', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPUQ0DZ_4y4GlK1fzKeOMzPeAed8IgJakKjchbZb4ngkRNihL1' },
-      { year:2011, titulo: 'Godzilla', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3YrHdrz4MTf2DcfRGatnQnGfxUz1JMG7PPX-THy-hc5PkKP7Q' },
-      { year:2016, titulo: 'Lion King', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJwrrD3K1_oKzR9MYSvgFVs4RHBFrmWELvsC9RILag22QpIi9y' }
+
+      new Pelicula('Spiderman 1', 2019, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSHQvqILPtAOdGe224vyidiP4gJh6Kkv6hoF8ZC5Vq68gGxgQPe'),
+      new Pelicula('Star Wars', 2018, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTCzXfErYlW_Af1vTVil5IGAjCcSlGwf79cSjt2FMXQyf0Rpzto'),
+      new Pelicula('Avengers', 2015, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ60i8ZIoMh38deQlfU0J19W-5GIzFYKC-vkDCEyvz34f0YEF2M'),
+      new Pelicula('Back to the future', 2010, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSPUQ0DZ_4y4GlK1fzKeOMzPeAed8IgJakKjchbZb4ngkRNihL1'),
+      new Pelicula('Lion King', 2016, 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJwrrD3K1_oKzR9MYSvgFVs4RHBFrmWELvsC9RILag22QpIi9y')
     ];
   }
 
---
+++ src/app/models/pelicula.model.ts
@@ -0,0 +1,9 @@
+export class Pelicula {
+
+    constructor(
+        public titulo: string,
+        public year: number,
+        public image: string
+    ){}
+
+}
