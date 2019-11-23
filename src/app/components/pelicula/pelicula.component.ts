0c8463bf034a6aecd719b14b2c5e94edf6a8bfaa
Dangzz <dan.gzz@outlook.com>
2019-11-22 19:54:11 -0600

    Componente pelicula

---
+++ src/app/components/pelicula/pelicula.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Pelicula } from 'src/app/models/pelicula.model';
+
+@Component({
+  selector: 'app-pelicula',
+  templateUrl: './pelicula.component.html',
+  styleUrls: ['./pelicula.component.css']
+})
+export class PeliculaComponent implements OnInit {
+
+  @Input() pelicula: Pelicula;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
